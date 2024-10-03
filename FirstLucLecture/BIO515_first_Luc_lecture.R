e5cbc99eaf80198e703dc5c9818fd6f3201da66e
MandaPandaTroll <viddadodda@gmail.com>
2024-10-03 14:20:24 +0200

    first commit

---
+++ FirstLucLecture/BIO515_first_Luc_lecture.R
@@ -0,0 +1,31 @@
+#BIO515 first Luc lecture
+
+
+#clearing workspace
+{
+  rm(list = ls())
+  gc()
+}
+
+
+#installing stuff
+{
+  #markdown
+  if(!require("rmarkdown") ){
+    install.packages("rmarkdown")
+  }
+  #tidyverse
+  if(!require("tidyverse") ){
+    install.packages("tidyverse")
+  }
+  #ggplot2
+  if(!require("ggplot2") ){
+    install.packages("ggplot2")
+  }
+}
+
+
+#load R script
+file.edit("Tidyverse_test.R")
+
+
